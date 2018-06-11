b261266ffada63396dab15c5c9f86ed47fe77ce2
XSL <3091167389@qq.com>
2018-06-11 21:57:37 +0800

    update

---
+++ app/src/main/java/com/bean/Customer.java
@@ -0,0 +1,90 @@
+package com.bean;
+
+/**
+ * Created by 16419 on 2018/6/11.
+ */
+
+public class Customer {
+
+    private String event;
+    private String firstname;
+    private String lastname;
+    private String isevg;
+    private String created;
+    private String updated;
+    private String email;
+    private String seat;
+    private String contactNo;
+
+    public String getEvent() {
+        return event;
+    }
+
+    public void setEvent(String event) {
+        this.event = event;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getIsevg() {
+        return isevg;
+    }
+
+    public void setIsevg(String isevg) {
+        this.isevg = isevg;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public String getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(String updated) {
+        this.updated = updated;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSeat() {
+        return seat;
+    }
+
+    public void setSeat(String seat) {
+        this.seat = seat;
+    }
+
+    public String getContactNo() {
+        return contactNo;
+    }
+
+    public void setContactNo(String contactNo) {
+        this.contactNo = contactNo;
+    }
+}
