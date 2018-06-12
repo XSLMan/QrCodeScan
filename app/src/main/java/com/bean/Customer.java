90833111103a51a3d903faf3377938f968ab0b24
homwu <719019727@qq.com>
2018-06-12 09:12:12 +0800

    46541

---
+++ app/src/main/java/com/bean/Customer.java
@@ -0,0 +1,125 @@
+package com.bean;
+
+/**
+ * Created by Administrator on 2018-06-11.
+ */
+
+public class Customer {
+    private String event;
+    private String firstname;
+    private String lastname;
+    private String company;
+    private String title;
+    private String email;
+    private String contactNo;
+    private String isveg;
+    private String meal;
+    private String url;
+    private String created;
+    private String updated;
+    private String seat;
+
+    public String getEvent() {
+        return event;
+    }
+
+    public void setEvent(String event) {
+        this.event = event;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContactNo() {
+        return contactNo;
+    }
+
+    public void setContactNo(String contactNo) {
+        this.contactNo = contactNo;
+    }
+
+    public String getIsveg() {
+        return isveg;
+    }
+
+    public void setIsveg(String isveg) {
+        this.isveg = isveg;
+    }
+
+    public String getMeal() {
+        return meal;
+    }
+
+    public void setMeal(String meal) {
+        this.meal = meal;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public String getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(String updated) {
+        this.updated = updated;
+    }
+
+    public String getSeat() {
+        return seat;
+    }
+
+    public void setSeat(String seat) {
+        this.seat = seat;
+    }
+}
